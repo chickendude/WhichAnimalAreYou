05aab1915380f5e3076da136943765d82593f8ed
chickendude <pokemaster103@gmail.com>
2016-10-09 21:54:45 +0800

    Added (Public Domain) pictures and icon files.

--- app/src/main/java/ch/ralena/whichanimalareyou/MainActivity.java
+++ app/src/main/java/ch/ralena/whichanimalareyou/MainActivity.java
@@ -3,6 +3,9 @@ package ch.ralena.whichanimalareyou;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+// all images but monkey, dolphin, elephant, panda, and tiger taken
+// from publicdomainpictures.net
+
 public class MainActivity extends AppCompatActivity {
 
 	@Override
